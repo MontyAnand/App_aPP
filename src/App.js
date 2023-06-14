d148fe1886de4f0c1bfaa853684dcc74742aebd2
MontyAnand <vk2818970@gmail.com>
2023-06-15 05:12:41 +0530

    fixed the socket error

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import React, { useEffect, useState } from "react";
 import Chat from "./chat";
 import Axios from 'axios';
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect();
 
 function App() {
   const [prevData, setPrevData] = useState([]);
